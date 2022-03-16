3ca884325c0b0c8383e501320b9b38053775cb09
Justin Truong <justin.truongt@yahoo.com>
2022-03-16 05:57:24 +0000

    images link and connection to other pages through index

--- index.js
+++ index.js
@@ -7,14 +7,24 @@ app.set('view engine','ejs');
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  var title  = 'Our Home Page';
+  var title  = 'Seahawks Home Page';
   res.render('pages/index',{title:title});
 });
 
-//about-us
-app.get('/about-us', (req, res) => {
-  var title  = 'Our Home Page';
-  res.render('pages/about-us',{title:title});
+//Info
+app.get('/Info', (req, res) => {
+  var title  = 'Info Page';
+  res.render('pages/Info',{title:title});
+});
+//Rivals
+app.get('/Rivals', (req, res) => {
+  var title  = 'Rivals Page';
+  res.render('pages/Rivals',{title:title});
+});
+//Superbowls
+app.get('/Superbowls', (req, res) => {
+  var title  = 'Superbowls Page';
+  res.render('pages/Superbowls',{title:title});
 });
 
 app.listen(port, () => {
